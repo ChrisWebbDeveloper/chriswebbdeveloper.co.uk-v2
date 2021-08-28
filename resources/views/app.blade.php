b6e766ea99a5c5501cc5d30f2aad48175dd1db6b
ChrisWebbDeveloper <webb.christopher@live.co.uk>
2021-08-28 13:10:00 +0100

    favicon and google analytics updated

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,28 +4,33 @@
         <title>Chris Webb Developer</title>
 
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="theme-color" content="#000000" />
         <meta name="title" content="Chris Webb Developer" />
-        <meta name="description" content="Web Development portfolio of Christopher Webb" />
-        <meta name="keywords" content="chris webb developer, chriswebbdeveloper, chris webb, christopher webb, christopher dean webb, chris dean webb, chriswebdeveloper, web developer, development, react, sass, php, laravel, javascript, typescript, kellydanielsphotography, nottingham, nottinghamshire, uk" />
+        <meta name="description" content="I am Chris Webb, a full-stack web developer based in Nottingham, UK. This is a portfolio of my work, using a variety of languages and frameworks, including ASP Net Core, PHP, Laravel, React, Typescript, SASS and MySQL" />
+        <meta name="keywords" content="chris webb developer, chriswebbdeveloper, chris webb, christopher webb, christopher dean webb, chris dean webb, chriswebdeveloper, dean webb developer, deanwebbdeveloper, deanwebdeveloper, web developer, development, react, sass, php, laravel, javascript, typescript, kellydanielsphotography, nottingham, nottinghamshire, uk" />
         <meta name="robots" content="index, follow" />
         <meta name="language" content="English" />
-        <meta name="author" content="Christopher Webb" />
+        <meta name="author" content="Chris Webb" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" type="image/ico" href="/favicon.ico" />
+        <!-- Favicon settings, provided by realfavicongenerator.net -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#030816">
+        <meta name="msapplication-TileColor" content="#030816">
+        <meta name="theme-color" content="#030816">
 
         <link href="/css/app.css" rel="stylesheet" />
 
         <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-38LZJWDFHN"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-FN7TQBJ0HM"></script>
         <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-            gtag('config', 'G-38LZJWDFHN');
+        gtag('config', 'G-FN7TQBJ0HM');
         </script>
     </head>
     <body>
